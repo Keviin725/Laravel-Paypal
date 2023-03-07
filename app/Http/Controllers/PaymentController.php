d2791226da9e61a4a447b3fcdd8d15279d2c6945
Kevin Machava <kevinmachava20@gmail.com>
2023-03-07 08:22:22 +0200

    chore: create model and controller

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    private $gateway;
+    public function __construct()
+    {
+        $this->gateway = Omnipay::create('PayPal_Rest');
+        $this->gateway->setClientId('PAYPAL_CLIENT_ID');
+        $this->gateway->setSecretId('PAYPAL_CLIENT_SECRET');
+        $this->gateway->setTestMode(true);
+    }
+
+    public function payment(Request $request){
+        try {
+            $response = $this->gateway->purchase(array(
+                'amount' => $request->amount,
+                'currency' => env('PAYPAL_CURRENCY'),
+                'returnUrl' => url('success'),
+                'cancelUrl' => url('error')
+            ));
+
+            if($response->isRedirect()){
+                $response->redirect();
+            }else {
+                return $response->getMessage();
+            }
+            
+        } catch (\Throwable $th) {
+            return $th->getMessage();
+        }
+    }
+}
